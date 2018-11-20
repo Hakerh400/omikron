c38f595ceb34a50121a93a8cdd1ef415248b4fcf
Thomas <hakerh403@gmail.com>
2018-11-20 13:47:29 +0100

    commit

---
+++ test/main.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const O = require('..');
+
+const cwd = __dirname;
+
+describe('Omikron', () => {
+  it('Works', () => {
+    // ok
+  });
+});
