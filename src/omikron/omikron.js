52eca0e4b077367d844997a74d003cfdb4b3567a
Thomas <hakerh403@gmail.com>
2019-08-30 09:50:23 +0200

    commit

--- src/omikron/omikron.js
+++ src/omikron/omikron.js
@@ -64,6 +64,142 @@ class Color extends Uint8ClampedArray{
   }
 }
 
+class ImageData{
+  constructor(g=null, clear=false){
+    this.g = null;
+
+    this.w = null;
+    this.h = null;
+
+    this.imgd = null;
+    this.d = null;
+
+    if(g !== null) this.fetch(g, clear);
+  }
+
+  setG(g){
+    if(g === this.g) return;
+
+    this.g = g;
+    this.w = g.canvas.width;
+    this.h = g.canvas.height;
+  }
+
+  fetch(g=this.g, clear=false){
+    if(g !== this.g) this.setG(g);
+
+    this.imgd = g.getImageData(0, 0, this.w, this.h);
+    this.d = this.imgd.data;
+
+    if(clear){
+      const d = this.d;
+      const len = d.length;
+
+      for(var i = 0; i < len; i++){
+        d[i] = (i & 3) < 3 ? 0 : 255;
+      }
+    }
+  }
+
+  put(g=this.g){
+    if(g !== this.g) this.setG(g);
+
+    this.g.putImageData(this.imgd, 0, 0);
+  }
+
+  get(x, y, col, includeAlpha){
+    const {w, h, d} = this;
+
+    if(x < 0) x = 0;
+    else if(x >= w) x = (w | 0) - 1 | 0
+    if(y < 0) y = 0;
+    else if(y >= h) y = (h | 0) - 1 | 0;
+
+    const i = (x | 0) + (y | 0) * (w | 0) << 2;
+
+    if(includeAlpha){
+      col[0] = d[i | 0] | 0;
+      col[1] = d[(i | 0) + 1 | 0] | 0;
+      col[2] = d[(i | 0) + 2 | 0] | 0;
+      col[3] = d[(i | 0) + 3 | 0] | 0;
+    }else{
+      col[0] = d[i | 0] | 0;
+      col[1] = d[(i | 0) + 1 | 0] | 0;
+      col[2] = d[(i | 0) + 2 | 0] | 0;
+    }
+
+    return col;
+  }
+
+  set(x, y, col, includeAlpha){
+    const {w, h, d} = this;
+
+    if(x < 0 || x >= w || y < 0 || y >= h)
+      return;
+
+    const i = (x | 0) + (y | 0) * (w | 0) << 2;
+
+    if(includeAlpha){
+      d[i | 0] = col[0] | 0;
+      d[(i | 0) + 1 | 0] = col[1] | 0;
+      d[(i | 0) + 2 | 0] = col[2] | 0;
+      d[(i | 0) + 3 | 0] = col[3] | 0;
+    }else{
+      d[i | 0] = col[0] | 0;
+      d[(i | 0) + 1 | 0] = col[1] | 0;
+      d[(i | 0) + 2 | 0] = col[2] | 0;
+    }
+  }
+
+  setRgb(x, y, r, g, b){
+    const {w, h, d} = this;
+
+    if(x < 0 || x >= w || y < 0 || y >= h)
+      return;
+
+    const i = (x | 0) + (y | 0) * (w | 0) << 2;
+
+    d[i | 0] = r | 0;
+    d[(i | 0) + 1 | 0] = g | 0;
+    d[(i | 0) + 2 | 0] = b | 0;
+  }
+
+  iter(func, includeAlpha=0){
+    const {w, h, d} = this;
+
+    if(includeAlpha){
+      for(var y = 0, i = 0; y < h; y++){
+        for(var x = 0; x < w; x++, i += 4){
+          var col = func(x | 0, y | 0, d[i | 0] | 0, d[(i | 0) + 1 | 0] | 0, d[(i | 0) + 2 | 0] | 0, d[(i | 0) + 3 | 0] | 0);
+
+          if(col){
+            d[i | 0] = col[0] | 0;
+            d[(i | 0) + 1 | 0] = col[1] | 0;
+            d[(i | 0) + 2 | 0] = col[2] | 0;
+            d[(i | 0) + 3 | 0] = col[3] | 0;
+          }
+        }
+      }
+    }else{
+      for(var y = 0, i = 0; y < h; y++){
+        for(var x = 0; x < w; x++, i += 4){
+          var col = func(x | 0, y | 0, d[i | 0] | 0, d[(i | 0) + 1 | 0] | 0, d[(i | 0) + 2 | 0] | 0);
+
+          if(col){
+            d[i | 0] = col[0] | 0;
+            d[(i | 0) + 1 | 0] = col[1] | 0;
+            d[(i | 0) + 2 | 0] = col[2] | 0;
+          }
+        }
+      }
+    }
+  }
+
+  iterate(func, includeAlpha){
+    this.iter(func, includeAlpha);
+  }
+}
+
 class EventEmitter{
   #ls = O.obj();
 
@@ -1654,6 +1790,7 @@ const O = {
   // Classes
 
   Color,
+  ImageData,
   EventEmitter,
   Grid,
   Map2D,
@@ -1925,7 +2062,9 @@ const O = {
     return parent.querySelectorAll(selector);
   },
 
-  ce(parent, tag, classNames=null){
+  ce(parent, tag=null, classNames=null){
+    if(tag === null) return O.doc.createElement(parent);
+
     const elem = O.doc.createElement(tag);
 
     if(parent !== null)
@@ -2003,7 +2142,6 @@ const O = {
 
     const w = O.iw;
     const h = O.ih;
-    const [wh, hh] = [w, h].map(a => a / 2);
 
     const canvas = O.ce(O.body, 'canvas');
     let g = canvas.getContext('2d');
@@ -2023,7 +2161,13 @@ const O = {
     if(enhanced)
       g = new O.EnhancedRenderingContext(g);
 
-    return {g, w, h, wh, hh};
+    return {
+      g, w, h,
+      wh : w / 2,
+      hh : h / 2,
+      w1: w - 1,
+      h1: h - 1,
+    };
   },
 
   /*
@@ -2454,6 +2598,11 @@ const O = {
   last(arr, defaultVal=null){
     if(arr.length === 0) return defaultVal;
     return arr[arr.length - 1];
+  },
+
+  setLast(arr, val){
+    arr[arr.length - 1] = val;
+    return arr;
   },
 
   fst(set, defaultVal){ return O.first(set, defaultVal); },
