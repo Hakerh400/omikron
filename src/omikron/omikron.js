ee1a25c0368c4e126fae81da89761f9154a7dffb
Thomas <hakerh403@gmail.com>
2021-07-05 00:05:49 +0200

    commit

--- src/omikron/omikron.js
+++ src/omikron/omikron.js
@@ -4665,6 +4665,71 @@ const O = {
 
   // Other functions
 
+  arrOrder: (() => {
+    const arr = (vals, id, dir=0) => {
+      const n = BigInt(vals.length);
+      const arr = [];
+
+      id = BigInt(id);
+
+      while(id !== 0n){
+        let val = vals[--id % n];
+        id /= n;
+
+        if(dir) arr.push(val);
+        else arr.unshift(val);
+      }
+
+      return arr;
+    };
+
+    const str = (chars, id, dir=0) => {
+      const n = BigInt(chars.length);
+      let str = '';
+
+      id = BigInt(id);
+
+      while(id !== 0n){
+        let val = chars[--id % n];
+        id /= n;
+
+        if(dir) str += val;
+        else str = val + str;
+      }
+
+      return str;
+    };
+
+    const id = (vals, arr, dir=0) => {
+      const n = BigInt(vals.length);
+      const len = arr.length;
+      const map = new Map();
+
+      let id = 0n;
+
+      let start = dir ? len - 1 : 0;
+      let end = dir ? -1 : len;
+      let d = dir ? -1 : 1;
+
+      vals.forEach((val, index) => {
+        map.set(val, BigInt(index));
+      });
+
+      for(let index = start; index !== end; index += d){
+        let val = arr[index];
+        id = id * n + map.get(val) + 1n;
+      }
+
+      return id;
+    };
+
+    return {
+      arr,
+      str,
+      id,
+    };
+  })(),
+
   *mapr(iterable, func){
     const arrNew = [];
     let i = 0;
